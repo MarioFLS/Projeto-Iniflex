f4c52d9264e563d890d32f8d061b2439b8ae45db
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-25 11:54:20 -0300

    feat: Formação da saída da data

--- src/base/EmployeeList.java
+++ src/base/EmployeeList.java
@@ -3,6 +3,7 @@ package base;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,17 +12,18 @@ import java.util.Locale;
 import entities.Employee;
 
 public class EmployeeList {
+  private final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
   private List<Employee> employeesList = new ArrayList<>(Arrays.asList(
-      new Employee("Maria", LocalDate.parse("2000-10-18"), BigDecimal.valueOf(2099.44), "Operador"),
-      new Employee("João", LocalDate.parse("1990-05-12"), BigDecimal.valueOf(2284.38), "Operador"),
-      new Employee("Caio", LocalDate.parse("1961-05-02"), BigDecimal.valueOf(9836.14), "Coordenador"),
-      new Employee("Miguel", LocalDate.parse("1988-10-14"), BigDecimal.valueOf(19119.88), "Diretor"),
-      new Employee("Alice", LocalDate.parse("1995-01-05"), BigDecimal.valueOf(2234.68), "Recepcionista"),
-      new Employee("Heitor", LocalDate.parse("1999-11-19"), BigDecimal.valueOf(1582.72), "Operador"),
-      new Employee("Arthur", LocalDate.parse("1993-03-31"), BigDecimal.valueOf(4071.84), "Contador"),
-      new Employee("Laura", LocalDate.parse("1994-07-08"), BigDecimal.valueOf(3017.45), "Gerente"),
-      new Employee("Heloísa", LocalDate.parse("2003-05-24"), BigDecimal.valueOf(1605.85), "Eletricista"),
-      new Employee("Helena", LocalDate.parse("1999-09-02"), BigDecimal.valueOf(2799.93), "Gerente")));
+      new Employee("Maria", LocalDate.parse("18/10/2000", fmt), BigDecimal.valueOf(2099.44), "Operador"),
+      new Employee("João", LocalDate.parse("12/05/1990", fmt), BigDecimal.valueOf(2284.38), "Operador"),
+      new Employee("Caio", LocalDate.parse("02/05/1961", fmt), BigDecimal.valueOf(9836.14), "Coordenador"),
+      new Employee("Miguel", LocalDate.parse("14/10/1988", fmt), BigDecimal.valueOf(19119.88), "Diretor"),
+      new Employee("Alice", LocalDate.parse("05/01/1995", fmt), BigDecimal.valueOf(2234.68), "Recepcionista"),
+      new Employee("Heitor", LocalDate.parse("19/11/1999", fmt), BigDecimal.valueOf(1582.72), "Operador"),
+      new Employee("Arthur", LocalDate.parse("31/03/1993", fmt), BigDecimal.valueOf(4071.84), "Contador"),
+      new Employee("Laura", LocalDate.parse("08/07/1994", fmt), BigDecimal.valueOf(3017.45), "Gerente"),
+      new Employee("Heloísa", LocalDate.parse("24/05/2003", fmt), BigDecimal.valueOf(1605.85), "Eletricista"),
+      new Employee("Helena", LocalDate.parse("02/09/1996", fmt), BigDecimal.valueOf(2799.93), "Gerente")));
 
   public void addEmploy(Employee employee) {
     employeesList.add(employee);
@@ -40,7 +42,7 @@ public class EmployeeList {
     System.out.println("-----------------------------------------------------------------------------");
     for (Employee employee : employeesList) {
       System.out.format("%10s\t %15s\t %10s\t %15s",
-          employee.getName(), employee.getBirthDate(), formatNumber.format(employee.getSalary()),
+          employee.getName(), employee.getBirthDate().format(fmt), formatNumber.format(employee.getSalary()),
           employee.getOccupation());
       System.out.println();
     }
