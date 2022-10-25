952f0ccdb9326b455070b0c7a6d00081ab00c160
MarioFLS <mariofernandolsantos@gmail.com>
2022-10-25 17:03:35 -0300

    feat: Chamada das funções

--- src/App.java
+++ src/App.java
@@ -3,20 +3,57 @@ import base.EmployeeList;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        EmployeeList b = new EmployeeList();
+        EmployeeList employees = new EmployeeList();
 
-        b.printEmployees();
+        employees.printEmployees();
 
-        System.out.println("Removendo João");
-
-        b.removeEmploy("João");
-
-        System.out.println("Aumentando Salário em 10%");
-
-        b.salaryIncrease(10.0);
-
+        System.out.println("=============================================================================");
         System.out.println();
 
-        b.printEmployees();
+        System.out.println("Removendo João");
+        System.out.println();
+
+        employees.removeEmploy("João");
+
+        employees.printEmployees();
+
+        System.out.println("=============================================================================");
+        System.out.println();
+
+        System.out.println("Aumentando Salário em 10%");
+        System.out.println();
+
+        employees.salaryIncrease(10.0);
+
+        employees.printEmployees();
+
+        System.out.println("=============================================================================");
+        System.out.println();
+
+        System.out.println("Função de cada funciário");
+        System.out.println();
+        employees.printOccupation();
+
+        System.out.println("=============================================================================");
+        System.out.println();
+
+        System.out.println("Funcionarios Aniversáriantes do mês de Outubro (10) e de Dezembro(12)");
+        System.out.println();
+
+        employees.printBirthday();
+
+        System.out.println("=============================================================================");
+        System.out.println();
+
+        System.out.println("Funcionarios Acima dos 18 anos");
+        System.out.println();
+        employees.printAdultEmployees();
+
+        System.out.println("=============================================================================");
+        System.out.println();
+
+        System.out.println("Funcionarios por Ordem Alfabética");
+        System.out.println();
+        employees.printAlphabeticalOrder();
     }
 }
